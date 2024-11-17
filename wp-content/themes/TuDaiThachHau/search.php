742d071ecf5f590efb29a15bfaaf5f7353be0cbf
nguyenhuyit2308 <huyreunglang123@gmail.com>
2024-11-17 20:40:43 +0700

    đã thêm file search

--- wp-content/themes/TuDaiThachHau/search.php
+++ wp-content/themes/TuDaiThachHau/search.php
@@ -1,6 +1,3 @@
-<form action="giuong.html" class="form-nav">
-    <input class="input-nav" type="text" placeholder="Tìm sản phẩm" required>
-    <button class="input-nav" type="submit">
-        <img src="<?php echo get_theme_file_uri() . '/assets/search.png'; ?>" alt="Tìm kiếm" width="20">
-    </button>
-</form>
+<?php
+// Template Name:
+echo do_shortcode('[aws_search_form placeholder="Tìm kiếm sản phẩm..."]');
